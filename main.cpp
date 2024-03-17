5b9cd1d2a5a377e72a79023f972002bbcc29bdb1
Ewan Green <ewangreen95@gmail.com>
2024-03-16 22:56:21 -0600

    add verbose

--- main.cpp
+++ main.cpp
@@ -6,34 +6,41 @@ using namespace std;
 using namespace Magick;
 
 #define ERROR(a) \
+    std::cerr << "fix-skin: " << a << std::endl; \
     std::cout << a << std::endl; \
     break;
+
 static constexpr const char g_help_text[] =
-    R"GORP(fix-skin: primitively adjust DDNet skins so that they will no longer error in the client
+    R"GORP(fix-skin: primitively adjust a DDNet skin so that it will no longer error in the client
 Usage:
-    fix-skin file:input file:output
+   fix-skin file:input file:output
+
+   -h,  --help       Show this message
+   -v,  --verbose    Output progress
 
 Return codes:
-   -*: Incorrect invocation
-    0: Success
-    1: Process error
+   -*   Incorrect invocation
+    0   Success
+    1   Process error
 )GORP";
 
 enum InvocationError {
+    UNKNOWN = -1,
     SANE = 0,
     ARGS_LT,
     ARGS_GT,
     INFILE_NE,
     OUTPATH_NE,
     OUTFILE_CONFLICT,
-    ARGS_UNKNOWN
 };
+
 using Argument = std::tuple<const std::string, const std::string>;
 constexpr const Argument s_help_arg_strs = std::make_tuple("-h", "--help");
 constexpr const Argument s_verbose_arg_strs = std::make_tuple("-v", "--verbose");
 constexpr const std::array<Argument, 2> s_args = {s_help_arg_strs, s_verbose_arg_strs};
 
 bool verbose = false;
+bool help_requested = false;
 std::vector<std::string> unknown_args;
 
 void print_help(char *argv[], InvocationError error = SANE)
@@ -47,13 +54,6 @@ void print_help(char *argv[], InvocationError error = SANE)
         ERROR("Not enough arguments.")
     case ARGS_GT:
         ERROR("Too many arguments.")
-    case ARGS_UNKNOWN: {
-        std::string str;
-        for (int i = unknown_args.size() - 1; i >= 0; i--) {
-            str += unknown_args.at(i) + (i == 0 ? "" : " ");
-        }
-        ERROR("Unknown argument[s]: " + str);
-    }
     case INFILE_NE:
         ERROR("Input file " + std::filesystem::path(argv[1]).string()
               + " is inaccessible or does not exist.")
@@ -62,65 +62,77 @@ void print_help(char *argv[], InvocationError error = SANE)
               + " is unreadable or does not exist.")
     case OUTFILE_CONFLICT:
         ERROR("Output file already exists.")
+    case UNKNOWN:
+        ERROR("Unknown error.")
     }
     std::cout << g_help_text << std::endl;
 }
 
 bool is_arg(const char *str, const Argument &arg)
 {
-    std::string lhs = std::get<0>(arg);
-    std::string rhs = std::get<1>(arg);
-    return lhs == str || rhs == str;
+    return str == std::get<0>(arg) || str == std::get<1>(arg);
 }
 
-bool is_arg(const char *str)
+const Argument *is_arg(const char *str)
 {
-    for (auto i = 0; i < s_args.size(); i++) {
-        if (is_arg(str, s_args[i]))
-            return true;
-    };
-    return false;
+    for (const auto &it : s_args) {
+        if (is_arg(str, it))
+            return &it;
+    }
+    return nullptr;
 }
 
-uint32_t roundDimension(int x, int multiple)
+const std::integral auto roundDimension(const std::integral auto x, const int multiple)
 {
-    int remainder = x % multiple;
-    if (remainder == 0)
-        return x;
-
-    return x + multiple - remainder;
+    const std::integral auto remainder = x % multiple;
+    return remainder ? x + multiple - remainder : x;
 }
 
 InvocationError sane(int argc, char *argv[])
 {
+    for (auto i = argc - 1; i > 0; i--) {
+        if (std::string_view(argv[i]).starts_with('-')) {
+            auto arg = is_arg(argv[i]);
+            if (arg) {
+                if (arg == &s_help_arg_strs) {
+                    help_requested = true;
+                    return SANE;
+                } else if (arg == &s_verbose_arg_strs) {
+                    verbose = true;
+                }
+            } else {
+                unknown_args.push_back(argv[i]);
+            }
+        }
+    }
+
+    if (!unknown_args.empty()) {
+        std::string str;
+        for (const auto &it : unknown_args) {
+            str += (str.empty() ? "" : ", ") + it;
+            std::cout << "Ignoring unknown argument(s) " << str << std::endl;
+        }
+    }
+
     switch (argc) {
     case 3:
-        if (std::string(argv[2]).starts_with('-') && !is_arg(argv[2])) {
-            unknown_args.push_back(argv[2]);
-        } else {
-            if (!std::filesystem::exists(argv[1]))
-                return INFILE_NE;
-            if (!std::filesystem::exists(std::filesystem::path(argv[2]).parent_path()))
-                return OUTPATH_NE;
-            if (std::filesystem::exists(argv[2]))
-                return OUTFILE_CONFLICT;
-        }
+        if (!std::filesystem::exists(argv[1]))
+            return INFILE_NE;
+        if (!std::filesystem::exists(std::filesystem::path(argv[2]).parent_path()))
+            return OUTPATH_NE;
+        if (std::filesystem::exists(argv[2]))
+            return OUTFILE_CONFLICT;
         [[fallthrough]];
     case 2:
-        if (std::string(argv[1]).starts_with('-') && !is_arg(argv[1])) {
-            unknown_args.push_back(argv[1]);
-        }
         break;
     case 1:
         [[fallthrough]];
-    case 0:
+    case 0: // Impossible
         return ARGS_LT;
     default:
         return ARGS_GT;
     }
 
-    if (!unknown_args.empty())
-        return ARGS_UNKNOWN;
     if (argc == 3)
         return SANE;
     return ARGS_LT;
@@ -129,7 +141,6 @@ InvocationError sane(int argc, char *argv[])
 int main(int argc, char *argv[])
 {
     InvocationError error = sane(argc, argv);
-    bool help_requested = argc >= 2 && is_arg(argv[1], s_help_arg_strs);
     if (error || help_requested) {
         if (help_requested)
             error = SANE;
@@ -151,8 +162,8 @@ int main(int argc, char *argv[])
         auto w = roundDimension(geometry.width(), 8);
         auto h = roundDimension(geometry.height(), 4);
 
-        if (!verbose) {
-            std::cout << std::to_string(geometry.width()) + "x" + std ::to_string(geometry.height())
+        if (verbose) {
+            std::cout << std::to_string(geometry.width()) + "x" + std::to_string(geometry.height())
                       << " -> " << std::to_string(w) + "x" + std ::to_string(h) << std::endl;
         }
 
@@ -160,12 +171,11 @@ int main(int argc, char *argv[])
         geometry.yOff(0);
         geometry.width(w);
         geometry.height(h);
+        geometry.aspect(true);
 
         image.alpha(true);
         image.depth(8);
         image.colorSpace(ColorspaceType::sRGBColorspace);
-
-        geometry.aspect(true);
         image.resize(geometry);
 
         // Write the image to a file
@@ -174,9 +184,7 @@ int main(int argc, char *argv[])
         image.write(output_file);
     } catch (Exception &error_) {
         cout << "Caught exception: " << error_.what() << endl;
-        return 1;
+        return UNKNOWN;
     }
     return 0;
 }
-
-// todo add verbose
