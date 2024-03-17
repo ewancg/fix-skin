f1461ae4817fb9e777de1942aaa074796e1d91b3
Ewan Green <ewangreen95@gmail.com>
2024-03-16 23:16:12 -0600

    clean

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
-#include "Magick++.h"
+﻿#include "Magick++.h"
 #include <filesystem>
 #include <iostream>
 
 using namespace std;
 using namespace Magick;
 
-#define ERROR(a) \
-    std::cerr << "fix-skin: " << a << std::endl; \
-    std::cout << a << std::endl; \
-    break;
+template<typename... Ts>
+static inline void error(const Ts... msg) noexcept
+{
+    std::cerr << "fix-skin: ";
+    (
+        [&] {
+            std::cerr << msg;
+            std::cout << msg;
+        }(),
+        ...);
+    std::cerr << std::endl;
+    std::cout << std::endl;
+}
 
 static constexpr const char g_help_text[] =
     R"GORP(fix-skin: primitively adjust a DDNet skin so that it will no longer error in the client
@@ -24,7 +33,7 @@ Return codes:
     1   Process error
 )GORP";
 
-enum InvocationError {
+enum ErrorStatus {
     UNKNOWN = -1,
     OK = 0,
     ARGS_LT,
@@ -43,27 +52,35 @@ bool verbose = false;
 bool help_requested = false;
 std::vector<std::string> unknown_args;
 
-void print_help(char *argv[], InvocationError error = OK)
+void print_help(char *argv[], ErrorStatus status = OK)
 {
-    switch (error) {
+    switch (status) {
     case OK:
         [[fallthrough]];
     default:
         break;
     case ARGS_LT:
-        ERROR("Not enough arguments.")
+        error("Not enough arguments.");
+        break;
     case ARGS_GT:
-        ERROR("Too many arguments.")
+        error("Too many arguments.");
+        break;
     case INFILE_NE:
-        ERROR("Input file " + std::filesystem::path(argv[1]).string()
-              + " is inaccessible or does not exist.")
+        error("Input file ",
+              std::filesystem::path(argv[1]).string(),
+              " is inaccessible or does not exist.");
+        break;
     case OUTPATH_NE:
-        ERROR("Output file directory " + std::filesystem::path(argv[2]).parent_path().string()
-              + " is unreadable or does not exist.")
+        error("Output file directory ",
+              std::filesystem::path(argv[2]).parent_path().string(),
+              " is unreadable or does not exist.");
+        break;
     case OUTFILE_CONFLICT:
-        ERROR("Output file already exists.")
+        error("Output file already exists.");
+        break;
     case UNKNOWN:
-        ERROR("Unknown error.")
+        error("Unknown error.");
+        break;
     }
     std::cout << g_help_text << std::endl;
 }
@@ -88,7 +105,7 @@ const std::integral auto roundDimension(const std::integral auto x, const int mu
     return remainder ? x + multiple - remainder : x;
 }
 
-InvocationError sane(int argc, char *argv[])
+ErrorStatus sane(int argc, char *argv[])
 {
     for (auto i = argc - 1; i > 0; i--) {
         if (std::string_view(argv[i]).starts_with('-')) {
@@ -140,51 +157,52 @@ InvocationError sane(int argc, char *argv[])
 
 int main(int argc, char *argv[])
 {
-    InvocationError error = sane(argc, argv);
-    if (error || help_requested) {
+    ErrorStatus status = sane(argc, argv);
+    if (status || help_requested) {
         if (help_requested)
-            error = OK;
-        print_help(argv, error);
-        return -error;
-    }
-    std::string input_file = std::filesystem::absolute(argv[1]).string(),
-                output_file = std::filesystem::absolute(argv[2]).string();
+            status = OK;
+        print_help(argv, status);
+    } else {
+        std::string input_file = std::filesystem::absolute(argv[1]).string(),
+                    output_file = std::filesystem::absolute(argv[2]).string();
 
-    InitializeMagick(*argv);
-    try {
-        Image image;
-        // Read a file into image object
-        if (verbose)
-            std::cout << "Reading from " << input_file << std::endl;
-        image.read(input_file);
-        auto geometry = image.size();
+        InitializeMagick(*argv);
+        try {
+            Image image;
+            // Read a file into image object
+            if (verbose)
+                std::cout << "Reading from " << input_file << std::endl;
+            image.read(input_file);
+            auto geometry = image.size();
 
-        auto w = roundDimension(geometry.width(), 8);
-        auto h = roundDimension(geometry.height(), 4);
+            auto w = roundDimension(geometry.width(), 8);
+            auto h = roundDimension(geometry.height(), 4);
 
-        if (verbose) {
-            std::cout << std::to_string(geometry.width()) + "x" + std::to_string(geometry.height())
-                      << " -> " << std::to_string(w) + "x" + std ::to_string(h) << std::endl;
+            if (verbose) {
+                std::cout << std::to_string(geometry.width()) + "x"
+                                 + std::to_string(geometry.height())
+                          << " -> " << std::to_string(w) + "x" + std ::to_string(h) << std::endl;
+            }
+
+            geometry.xOff(0);
+            geometry.yOff(0);
+            geometry.width(w);
+            geometry.height(h);
+            geometry.aspect(true);
+
+            image.alpha(true);
+            image.depth(8);
+            image.colorSpace(ColorspaceType::sRGBColorspace);
+            image.resize(geometry);
+
+            // Write the image to a file
+            if (verbose)
+                std::cout << "Writing to " << output_file << std::endl;
+            image.write(output_file);
+        } catch (Exception &e) {
+            status = UNKNOWN;
+            error("Caught exception: ", e.what());
         }
-
-        geometry.xOff(0);
-        geometry.yOff(0);
-        geometry.width(w);
-        geometry.height(h);
-        geometry.aspect(true);
-
-        image.alpha(true);
-        image.depth(8);
-        image.colorSpace(ColorspaceType::sRGBColorspace);
-        image.resize(geometry);
-
-        // Write the image to a file
-        if (verbose)
-            std::cout << "Writing to " << output_file << std::endl;
-        image.write(output_file);
-    } catch (Exception &error_) {
-        cout << "Caught exception: " << error_.what() << endl;
-        error = UNKNOWN;
     }
-    return -error;
+    return -status;
 }
