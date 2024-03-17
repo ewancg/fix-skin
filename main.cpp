deeece0440919019c682b75cdf5e8885ffece5d5
Ewan Green <ewangreen95@gmail.com>
2024-03-16 23:17:55 -0600

    clean

--- main.cpp
+++ main.cpp
@@ -3,20 +3,39 @@
 #include <iostream>
 
 using namespace std;
-using namespace Magick;
+
+std::time_t g_t = std::time(nullptr);
+std::tm g_tm{};
+
+enum LogLevel { INFO, WARN, FATAL };
+template<typename... Ts>
+static void log(LogLevel level, const Ts... msg) noexcept
+{
+    auto &stream = (level == INFO) ? std::cout : std::cerr;
+    ([&] { stream << msg; }(), ...);
+    std::endl(stream);
+}
 
 template<typename... Ts>
 static inline void error(const Ts... msg) noexcept
 {
-    std::cerr << "fix-skin: ";
-    (
-        [&] {
-            std::cerr << msg;
-            std::cout << msg;
-        }(),
-        ...);
-    std::cerr << std::endl;
-    std::cout << std::endl;
+    g_tm = *std::localtime(&g_t);
+    std::cerr << "Error [" << std::put_time(&g_tm, "%X") << "]: ";
+    log(LogLevel::FATAL, msg...);
+}
+
+template<typename... Ts>
+static inline void warn(const Ts... msg) noexcept
+{
+    g_tm = *std::localtime(&g_t);
+    std::cerr << "Warning [" << std::put_time(&g_tm, "%X") << "]: ";
+    log(LogLevel::WARN, msg...);
+}
+
+template<typename... Ts>
+static inline void info(const Ts... msg) noexcept
+{
+    log(LogLevel::INFO, msg...);
 }
 
 static constexpr const char g_help_text[] =
@@ -28,29 +47,29 @@ Usage:
    -v,  --verbose    Output progress
 
 Return codes:
-   -*   Incorrect invocation
+   -*   Bad invocation
     0   Success
-    1   Process error
+    1   Unknown error
 )GORP";
 
-enum ErrorStatus {
-    UNKNOWN = -1,
-    OK = 0,
-    ARGS_LT,
-    ARGS_GT,
-    INFILE_NE,
-    OUTPATH_NE,
-    OUTFILE_CONFLICT,
-};
-
 using Argument = std::tuple<const std::string, const std::string>;
-constexpr const Argument s_help_arg_strs = std::make_tuple("-h", "--help");
-constexpr const Argument s_verbose_arg_strs = std::make_tuple("-v", "--verbose");
-constexpr const std::array<Argument, 2> s_args = {s_help_arg_strs, s_verbose_arg_strs};
+constexpr const Argument g_help_arg_strs = std::make_tuple("-h", "--help");
+constexpr const Argument g_verbose_arg_strs = std::make_tuple("-v", "--verbose");
+constexpr const std::array<Argument, 2> s_args = {g_help_arg_strs, g_verbose_arg_strs};
 
-bool verbose = false;
-bool help_requested = false;
-std::vector<std::string> unknown_args;
+bool g_verbose = false;
+bool g_help_requested = false;
+std::vector<std::string> g_unknown_args;
+
+enum ErrorStatus {
+    OK = 0,
+    ERROR_UNKNOWN = -1,
+    ERROR_ARGS_LT = 1,
+    ERROR_ARGS_GT,
+    ERROR_INFILE_NE,
+    ERROR_OUTPATH_NE,
+    ERROR_OUTFILE_CONFLICT,
+};
 
 void print_help(char *argv[], ErrorStatus status = OK)
 {
@@ -59,28 +78,28 @@ void print_help(char *argv[], ErrorStatus status = OK)
         [[fallthrough]];
     default:
         break;
-    case ARGS_LT:
+    case ERROR_ARGS_LT:
         error("Not enough arguments.");
         break;
-    case ARGS_GT:
+    case ERROR_ARGS_GT:
         error("Too many arguments.");
         break;
-    case INFILE_NE:
+    case ERROR_INFILE_NE:
         error("Input file ",
               std::filesystem::path(argv[1]).string(),
               " is inaccessible or does not exist.");
         break;
-    case OUTPATH_NE:
+    case ERROR_OUTPATH_NE:
         error("Output file directory ",
               std::filesystem::path(argv[2]).parent_path().string(),
               " is unreadable or does not exist.");
         break;
-    case OUTFILE_CONFLICT:
+    case ERROR_OUTFILE_CONFLICT:
         error("Output file already exists.");
         break;
-    case UNKNOWN:
+    case ERROR_UNKNOWN:
         error("Unknown error.");
-        break;
+        return;
     }
     std::cout << g_help_text << std::endl;
 }
@@ -99,7 +118,7 @@ const Argument *is_arg(const char *str)
     return nullptr;
 }
 
-const std::integral auto roundDimension(const std::integral auto x, const int multiple)
+const std::integral auto next_multiple(const std::integral auto x, const int multiple)
 {
     const std::integral auto remainder = x % multiple;
     return remainder ? x + multiple - remainder : x;
@@ -111,74 +130,71 @@ ErrorStatus sane(int argc, char *argv[])
         if (std::string_view(argv[i]).starts_with('-')) {
             auto arg = is_arg(argv[i]);
             if (arg) {
-                if (arg == &s_help_arg_strs) {
-                    help_requested = true;
+                if (arg == &g_help_arg_strs) {
+                    g_help_requested = true;
                     return OK;
-                } else if (arg == &s_verbose_arg_strs) {
-                    verbose = true;
+                } else if (arg == &g_verbose_arg_strs) {
+                    g_verbose = true;
                 }
             } else {
-                unknown_args.push_back(argv[i]);
+                g_unknown_args.push_back(argv[i]);
             }
         }
     }
 
-    if (!unknown_args.empty()) {
+    if (!g_unknown_args.empty()) {
         std::string str;
-        for (const auto &it : unknown_args) {
+        for (const auto &it : g_unknown_args) {
             str += (str.empty() ? "" : ", ") + it;
-            std::cout << "Ignoring unknown argument(s) " << str << std::endl;
         }
+        warn("Ignoring unknown argument(s) ", str);
     }
 
     switch (argc) {
     case 3:
         if (!std::filesystem::exists(argv[1]))
-            return INFILE_NE;
+            return ERROR_INFILE_NE;
         if (!std::filesystem::exists(std::filesystem::path(argv[2]).parent_path()))
-            return OUTPATH_NE;
+            return ERROR_OUTPATH_NE;
         if (std::filesystem::exists(argv[2]))
-            return OUTFILE_CONFLICT;
-        [[fallthrough]];
+            return ERROR_OUTFILE_CONFLICT;
+        return OK;
     case 2:
-        break;
+        [[fallthrough]];
     case 1:
         [[fallthrough]];
     case 0: // Impossible
-        return ARGS_LT;
+        return ERROR_ARGS_LT;
     default:
-        return ARGS_GT;
+        return ERROR_ARGS_GT;
     }
-
-    if (argc == 3)
-        return OK;
-    return ARGS_LT;
 }
 
 int main(int argc, char *argv[])
 {
     ErrorStatus status = sane(argc, argv);
-    if (status || help_requested) {
-        if (help_requested)
+    if (status || g_help_requested) {
+        if (g_help_requested)
             status = OK;
         print_help(argv, status);
     } else {
         std::string input_file = std::filesystem::absolute(argv[1]).string(),
                     output_file = std::filesystem::absolute(argv[2]).string();
 
+        using namespace Magick;
         InitializeMagick(*argv);
         try {
             Image image;
             // Read a file into image object
-            if (verbose)
+            if (g_verbose)
                 std::cout << "Reading from " << input_file << std::endl;
             image.read(input_file);
             auto geometry = image.size();
 
-            auto w = roundDimension(geometry.width(), 8);
-            auto h = roundDimension(geometry.height(), 4);
+            auto w = next_multiple(geometry.width(), 8);
+            auto h = next_multiple(geometry.height(), 4);
 
-            if (verbose) {
+            if (g_verbose) {
                 std::cout << std::to_string(geometry.width()) + "x"
                                  + std::to_string(geometry.height())
                           << " -> " << std::to_string(w) + "x" + std ::to_string(h) << std::endl;
@@ -190,18 +206,17 @@ int main(int argc, char *argv[])
             geometry.height(h);
             geometry.aspect(true);
 
-            image.alpha(true);
             image.depth(8);
             image.colorSpace(ColorspaceType::sRGBColorspace);
             image.resize(geometry);
 
             // Write the image to a file
-            if (verbose)
+            if (g_verbose)
                 std::cout << "Writing to " << output_file << std::endl;
             image.write(output_file);
         } catch (Exception &e) {
-            status = UNKNOWN;
-            error("Caught exception: ", e.what());
+            status = ERROR_UNKNOWN;
+            error(e.what());
         }
     }
     return -status;
